fa4873708565d0e017d99749a8753f372f60e1a2
kamangir <arash@kamangir.net>
2025-03-11 00:22:50 -0700

    ++ - kamangir/bolt#746

--- blue_gan/README.py
+++ blue_gan/README.py
@@ -12,7 +12,12 @@ items = README.Items(
             "marquee": "https://github.com/eriklindernoren/PyTorch-GAN/raw/master/assets/logo.png",
             "description": "Code base.",
             "url": "https://github.com/eriklindernoren/PyTorch-GAN",
-        }
+        },
+        {
+            "name": "What is a GAN?",
+            "marquee": "https://d2908q01vomqb2.cloudfront.net/f1f836cb4ea6efb2a0b1b99f41ad8b103eff4b59/2021/11/11/ML-6149-image025.jpg",
+            "url": "https://aws.amazon.com/what-is/gan/",
+        },
     ]
 )
 
