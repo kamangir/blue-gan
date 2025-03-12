92ab0382ba8b8936d494dbc8407ff2c3b2f8ac77
kamangir <arash@kamangir.net>
2025-03-11 21:59:09 -0700

    wip - kamangir/bolt#746

---
+++ blue_gan/.abcli/stylegan2_pytorch.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+function blue_gan_stylegan2_pytorch() {
+    local options=$1
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
+    local do_upload=$(abcli_option_int "$options" upload $(abcli_not $do_dryrun))
+
+    local object_name=$(abcli_clarify_object $2 stylegan2_pytorch-$(abcli_string_timestamp_short))
+
+    abcli_eval dryrun=$do_dryrun \
+        stylegan2_pytorch \
+        --data $ABCLI_OBJECT_ROOT/$object_name \
+        "${@:3}"
+    [[ $? -ne 0 ]] && return 1
+
+    [[ "$do_upload" == 1 ]] &&
+        abcli_upload - $object_name
+}
